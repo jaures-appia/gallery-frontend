a6935d0a237666af13a134062078e42ad2185533
jean-jaures <appia.ngouan@uvci.edu.ci>
2020-09-10 14:26:38 +0200

    create about page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <header>
             <!--Navbar -->
             <nav class="mb-1 navbar navbar-expand-lg navbar-dark blue">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <router-link class="navbar-brand" to="#">Pro-gallery</router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
                 aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -16,11 +16,9 @@
                 <ul class="navbar-nav ml-auto nav-flex-icons">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/my-photos">Photos</router-link>
-                        <!-- <a class="nav-link" href="index2.html">Photos</a> -->
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" to="/add-photo">Add Photo</router-link>
-                        <!-- <a class="nav-link" href="/add-photo">Add Photo</a> -->
                     </li>
                 
                     <li class="nav-item dropdown">
@@ -33,7 +31,6 @@
                     <div class="dropdown-menu dropdown-menu-right dropdown-default"
                         aria-labelledby="navbarDropdownMenuLink-333">
                         <router-link class="dropdown-item" to="/profile">my profile</router-link>
-                        <!-- <a class="dropdown-item" href="profile.html">my profile</a> -->
                         <a class="dropdown-item" href="#">Logout</a>
                     </div>
                     </li>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,91 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container my-5 py-5 z-depth-1">
+
+    <!--Section: Content-->
+    <section class="px-md-5 mx-md-5 text-center dark-grey-text">
+
+      <!--Grid row-->
+      <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-6 mb-4 mb-md-0">
+
+          <h3 class="font-weight-bold">Pro Gallery</h3>
+
+          <p class="text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente
+            molestiae
+            numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.</p>
+
+          <hr class="my-2">
+
+          <p class="font-weight-bold">Contact us</p>
+
+           <form class="" action="">
+
+        <div class="row">
+          <div class="col-12 mb-4">
+
+            <div class="card">
+              <div class="card-body px-4">
+
+                <!-- Name -->
+                <div class="md-form md-outline mt-0">
+                  <input type="text" id="name" class="form-control">
+                  <label for="name">Your Name</label>
+                </div>
+                <!-- Email -->
+                <div class="md-form md-outline">
+                  <input type="text" id="email" class="form-control">
+                  <label for="email">Your Email Address</label>
+                </div>
+                <!-- Message -->
+                <div class="md-form md-outline">
+                  <textarea id="message" class="md-textarea form-control" rows="3"></textarea>
+                  <label for="message">Your Message</label>
+                </div>
+
+                <button type="submit" class="btn btn-primary btn-md btn-block ml-0 mb-0">Submit inquiry</button>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </form>
+
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-5 mb-4 mb-md-0">
+
+          <img :src="imgAbout" class="img-fluid" alt="">
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Content-->
+
+  </div>
   </div>
 </template>
+
+<script>
+	import imgAbout from '../assets/undraw_online_gallery_dmv3.svg'
+	export default{
+		name: 'About',
+		data(){
+			return{
+				imgAbout: imgAbout,
+			}
+		}
+	}
+</script>
